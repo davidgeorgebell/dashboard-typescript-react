9f7afd7b8f2dd7e593e1e432549b4e7edb750970
davidgeorgebell <bell.dave93@gmail.com>
2020-11-26 09:00:56 +0800

    Configs

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Dave's Dashboard</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>Hello</main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
