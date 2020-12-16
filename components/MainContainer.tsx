27292ea9b6970591628bddd63875b9bb558989d5
davidgeorgebell <bell.dave93@gmail.com>
2020-12-16 09:40:30 +0800

    Styling

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -32,20 +32,37 @@ export const MainContainer = ({ location, time }: MainContainerProps) => {
   const daysLeftInYear = getDaysLeftInYear(year);
 
   return (
-    <div>
-      <h1>
-        {formatTime(time.datetime)}
-        <span>{time.abbreviation}</span>
-      </h1>
-      <h2>
-        Days remaining in {year}: {daysLeftInYear}
-      </h2>
-      <p>{location.timeZone}</p>
-      <p>{location.countryName}</p>
-      <div>
-        <p>Day of Year {time.dayOfYear}</p>
-        <p>Day of Week {time.dayOfWeek}</p>
-        <p>Week number {time.weekNumber}</p>
+    <div className='h-screen max-w-5xl flex flex-col justify-evenly mx-auto container px-10'>
+      <div className='flex justify-between'>
+        <div>
+          <h1 className='text-8xl font-black'>
+            {formatTime(time.datetime)}
+            <span className='text-2xl font-normal'>{time.abbreviation}</span>
+          </h1>
+          <p className='uppercase text-xl'>in {location.countryName}</p>
+        </div>
+        <div>
+          <p className='text-xl font-normal'>TIMEZONE</p>
+          <h3 className='text-6xl font-black'>{location.timeZone}</h3>
+        </div>
+      </div>
+      <div className='flex justify-between mb-10'>
+        <div className='text-center'>
+          <p className='text-xl'>DAYS REMAINING IN {year}</p>
+          <h3 className='font-black text-6xl'>{daysLeftInYear}</h3>
+        </div>
+        <div className='text-center'>
+          <p className='text-xl'>DAY OF YEAR </p>
+          <h3 className='font-black text-6xl'>{time.dayOfYear}</h3>
+        </div>
+        <div className='text-center'>
+          <p className='text-xl'>DAY OF WEEK </p>
+          <h3 className='font-black text-6xl'>{time.dayOfWeek}</h3>
+        </div>
+        <div className='text-center'>
+          <p className='text-xl'>WEEK NUMBER </p>
+          <h3 className='font-black text-6xl'>{time.weekNumber}</h3>
+        </div>
       </div>
     </div>
   );
